5fe1717b341b6ebb65964edb0e3ca537fc4bb2ca
Luiz-Carlos-de-Lima <luiz.carlos199.lcdl@gmail.com>
2021-12-07 10:48:57 -0300

    implementa o componente cronometro

--- lib/app/pages/pomodoro_page.dart
+++ lib/app/pages/pomodoro_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pomodoro/app/components/cronometro.dart';
 import 'package:pomodoro/app/components/entrada_tempo.dart';
 
 class PomodoroPage extends StatelessWidget {
@@ -9,14 +10,18 @@ class PomodoroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          const Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const <Widget>[
-              EntradaTempo(valor: 25, titulo: 'Trabalho'),
-              EntradaTempo(valor: 5, titulo: 'Descanso'),
-            ],
+          Expanded(child: Cronometro()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const <Widget>[
+                EntradaTempo(valor: 25, titulo: 'Trabalho'),
+                EntradaTempo(valor: 5, titulo: 'Descanso'),
+              ],
+            ),
           ),
         ],
       ),
